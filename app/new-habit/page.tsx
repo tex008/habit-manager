8dd9bf2b9414616a2bfe182f2d25dc7952220571
tex008 <texera009@gmail.com>
2023-08-17 22:23:08 -0300

    feat: create form to create new habit

--- app/new-habit/page.tsx
+++ app/new-habit/page.tsx
@@ -1,12 +1,22 @@
 export default function NewHabitPage() {
+  async function newHabit(formData: FormData) {
+    "use server";
+
+    const habit = formData.get("habit");
+
+    console.log("habit", habit)
+  }
+
+
+
   return (
     <main className="container relative flex flex-col gap-8 px-12 pt-16">
       <h1 className="text-4xl font-light text-center text-white font-display">
         New Habit
       </h1>
-      <form action="" className="flex flex-col gap-4 mt-4">
+      <form action={newHabit} className="flex flex-col gap-4 mt-4">
         <input type="text" name="habit" id="habit" className="p-2 font-sans text-xl text-white rounded-md bg-neutral-800"/>
-        <button className="bg-[#45EDAD] font-display text-neutral-900 font-normal text-2xl p-2 rounded-md mt-8">
+        <button type="submit" className="bg-[#45EDAD] font-display text-neutral-900 font-normal text-2xl p-2 rounded-md mt-8">
           cadastrar
         </button>
         <button className="bg-neutral-800 text-[#F85858] font-display font-normal text-2xl p-2 rounded-md">
